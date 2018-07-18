0075942720cb2cf5ddc9e9d48b2abb2aa3499422
Yasir Al Shekerchi <yalshekerchi@outlook.com>
2018-07-17 22:53:34 -0400

    Started work on front-end. Implemented login/logout UI

---
+++ client/src/components/Header.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return null;
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>;
+      default:
+        return (
+          <span>
+            <li>Hello {this.props.auth.displayName}</li>
+            <li><a href="/api/logout">Logout</a></li>
+          </span>
+        );
+    }
+  }
+
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
+          <ul id="nav-mobile" className="right">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
