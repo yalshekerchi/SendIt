cd371f1f9d808ed314725c5abd230c99e2a11bbf
Yasir Al Shekerchi <yalshekerchi@outlook.com>
2018-07-20 22:01:10 -0400

    Implented Stripe billing and configurated heroku production scripts

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import Payments from './Payments';
+
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
@@ -11,10 +13,11 @@ class Header extends Component {
         return <li><a href="/auth/google">Login With Google</a></li>;
       default:
         return (
-          <span>
-            <li>Hello {this.props.auth.displayName}</li>
-            <li><a href="/api/logout">Logout</a></li>
-          </span>
+          [
+            <li key='1'><Payments /></li>,
+            <li key='3' style={{ margin: '0 10px' }}>Credits: {this.props.auth.credits}</li>,
+            <li key='2'><a href="/api/logout">Logout</a></li>
+          ]
         );
     }
   }
